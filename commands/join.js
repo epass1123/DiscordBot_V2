bf6461dd263bbbd04de7394dd87950c3202cb386
epass1123 <404.notfound@kakao.com>
2022-02-17 13:42:31 +0900

    .

--- commands/join.js
+++ commands/join.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
 
 require('dotenv').config();
 
@@ -10,12 +10,9 @@ module.exports = {
 		.setDescription('꿀벌봇이 음성채널에 참가한다.'),
 	async execute(interaction) {
 
-		const connection = joinVoiceChannel({
-			channelId: process.env.CLIENT_ID,
-			guildId: process.env.GUILD_ID,
-			adapterCreator: channel.guild.voiceAdapterCreator,
-		});
-
+		const connection = getVoiceConnection(myVoiceChannel.guild.id);
+		connection.on();
+		
 		await interaction.reply('안녕하세요!');
 	},
 };
